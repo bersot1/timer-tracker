d6ae9928f8a16c665327a453a94fe0e2bcd34b3f
Lucas Marinho <lucasbmarinho9@gmail.com>
2023-02-20 18:43:17 +0000

    update code to track data from IOS and android

--- src/extension.ts
+++ src/extension.ts
@@ -7,22 +7,57 @@ const path = require('path');
 
 
 export function activate(context: vscode.ExtensionContext) {
-	let startTime: Date;
 
-	context.subscriptions.push(vscode.debug.onDidStartDebugSession(session => {
-		startTime = new Date();
+	const regex = /\d+(\.\d+)?/g;
+	const regexGetOS = /(ios|android)/i;
+
+	const buildDoneLogiOS = 'Xcode build done.';
+	const buildDoneLogAndroid = 'Built build/app/outputs/flutter-apk';
+
+	let buildStartTime: Date | undefined;
+
+	let programName = '';
+	let deviceName = '';
+
+
+	context.subscriptions.push(vscode.debug.onDidReceiveDebugSessionCustomEvent(event => {
+
+		console.log(event);
+
+		buildStartTime = new Date;
+
+		if (event.body.message.includes(buildDoneLogiOS) || event.body.message.includes(buildDoneLogAndroid)) {
+
+			deviceName = event.session.configuration.deviceName.match(regexGetOS)[0];
+			programName = event.session.configuration.cwd;
+
+			if (deviceName == 'ios') {
+
+				const result = event.body.message.match(regex);
+				saveTimeSpent(result[0], 'Start Debugging (F5)');
+
+			} else {
+
+				let endTime = new Date();
+				let timeSpent = (endTime.getTime() - buildStartTime.getTime()) / 1000;
+
+				saveTimeSpent(timeSpent.toString(), 'Start Debugging (F5)')
+
+			}
+		}
 	}))
 
-	context.subscriptions.push(vscode.debug.onDidTerminateDebugSession(session => {
-		saveTimeSpent(startTime, 'debug');
-	}));
 
-	function saveTimeSpent(startTime: Date, commandName: string) {
-		let endTime = new Date();
-		let timeSpent = endTime.getTime() - startTime.getTime();
+	function saveTimeSpent(timeSpent: string, commandName: string) {
 		const today = new Date().toISOString();
 
-		const data = { today, timeSpent, commandName };
+		const data = {
+			"datetime": today,
+			"command": commandName,
+			"duration (seconds)": timeSpent,
+			"project": programName,
+			"device": deviceName,
+		};
 
 		const filePath = path.join(os.homedir(), '.builds.json');
 
